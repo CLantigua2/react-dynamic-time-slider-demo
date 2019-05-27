e89d00b9f5171e18b3e36a70cfa790c891005b7d
Carlos Lantigua <clantigua954@gmail.com>
2019-05-27 12:09:23 -0400

    updating app to use proper depenency package, the old one was broken

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Slider from 'react-modern-time-slider'
+import Slider from 'react-dynamic-time-slider'
 import { container, install } from './Style'
 
 
